770807afed01716ebe5794877955fd2495ce4616
Thomas Klinger <Thomas.Klinger@otto.de>
2020-06-15 21:14:43 +0200

    chore: run shfmt

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,23 +7,23 @@ set -o pipefail
 OS_PLATFORM=$(uname)
 
 function main() {
-    echo "Installing Bazel for platform <$OS_PLATFORM>"
+  echo "Installing Bazel for platform <$OS_PLATFORM>"
 
-    case $OS_PLATFORM in
-        Darwin)
-            brew install bazel
-            ;;
-        Linux)
-            curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-            echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-            apt-get update -y
-            apt-get install -y bazel
-            ;;
-        *)
-            echo "invalid platform ${OS_PLATFORM}"
-            exit 1
-            ;;
-    esac
+  case $OS_PLATFORM in
+    Darwin)
+      brew install bazel
+      ;;
+    Linux)
+      curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+      echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+      apt-get update -y
+      apt-get install -y bazel
+      ;;
+    *)
+      echo "invalid platform ${OS_PLATFORM}"
+      exit 1
+      ;;
+  esac
 }
 
 main $@
