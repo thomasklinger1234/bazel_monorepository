a999cef7666f5307076230eb5e260cd5e71b1fe2
Thomas Klinger <Thomas.Klinger@otto.de>
2020-06-15 20:40:29 +0200

    fix: become sudo when bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,8 +14,8 @@ function main() {
             brew install bazel
             ;;
         Linux)
-            curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-            echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+            curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+            echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
             apt-get update -y
             apt-get install -y bazel
             ;;
