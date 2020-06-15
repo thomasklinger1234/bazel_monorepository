0802fc516804c42751453b0cdcd6c94933c4f0d6
Thomas Klinger <Thomas.Klinger@otto.de>
2020-06-15 20:38:58 +0200

    refactor: use bootstrap script to align local and ci setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,6 @@ function main() {
             brew install bazel
             ;;
         Linux)
-            apt-get install -y install curl gnupg
             curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
             echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
             apt-get update -y
